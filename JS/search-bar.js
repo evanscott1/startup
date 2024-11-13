07a7cb64f2c706cfbaab2af8493bcc50472c9ec7
evanscott1 <157188169+evanscott1@users.noreply.github.com>
2024-11-12 21:23:42 -0700

    Chore: Cleaning up previous directory

--- JS/search-bar.js
+++
@@ -1,8 +0,0 @@
-const userInput = document.getElementById('user-input');
-
-userInput.addEventListener('input', function() {
-  // Reset the height to 'auto' to allow shrinking if necessary
-  this.style.height = 'auto';
-  // Set the height to the scroll height (the full height of the content)
-  this.style.height = `${this.scrollHeight}px`;
-});
